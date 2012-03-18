1e73a25b1f11ba41f4fafb12099885182f11f273
Valeriu Cucu <valeriu.ciprian.cucu@gmail.com>
2012-03-18 23:41:41 +0200

    fixed file upload thanks to wsdookadr

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -1,37 +1,26 @@
 function uploadAsync(elem, url) {
   var fileForm = document.getElementById(elem),
-  supressEvent = function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-  },
-  handleUpload = function (e) {
-    console.log("file upload handled");
-    supressEvent(e);
+	supressEvent = function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+	},
+	handleUpload = function (e) {
+		console.log("file upload handled");
+		supressEvent(e);
 
-<<<<<<< HEAD
-			var file = e.dataTransfer.files[0],
-				xhr = new XMLHttpRequest(),
-				name = file.name || file.fileName;
-			xhr.open("POST", url + name, true);
-			xhr.setRequestHeader("X-File-Name", name);
-			xhr.send(file);
-		}
-=======
-    var file = e.dataTransfer.files[0],
-    xhr = new XMLHttpRequest(),
-    name = file.name || file.fileName;
-    console.log(file);
+		var file = e.dataTransfer.files[0],
+		xhr = new XMLHttpRequest(),
+		name = file.name || file.fileName;
+		console.log(file);
 
->>>>>>> ea691bc383a8bef2c847e724bba3813a179ca001
-
-    xhr.open("POST", url + name, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("X-File-Name", file.fileName);
-    xhr.setRequestHeader("X-File-Size", file.fileSize);
-    xhr.setRequestHeader("X-File-Type", file.type);
-    xhr.setRequestHeader("Content-Length", file.size);
-    xhr.setRequestHeader("Connection", "close");
-    xhr.send(file);
+		xhr.open("POST", url + name, true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.setRequestHeader("X-File-Name", file.fileName);
+		xhr.setRequestHeader("X-File-Size", file.fileSize);
+		xhr.setRequestHeader("X-File-Type", file.type);
+		xhr.setRequestHeader("Content-Length", file.size);
+		xhr.setRequestHeader("Connection", "close");
+		xhr.send(file);
   }
 
 	fileForm.addEventListener('dragenter', supressEvent, false);
--- routes/index.pl
+++ routes/index.pl
@@ -5,17 +5,17 @@ get '/' => sub {
 
 post '/:file' => sub {
 	my $self = shift;
-  my $UPLOAD_DIR = "./public/upload";
-  `mkdir $UPLOAD_DIR` unless -d $UPLOAD_DIR;
+	my $UPLOAD_DIR = "./public/upload";
+	`mkdir $UPLOAD_DIR` unless -d $UPLOAD_DIR;
 
-  my $headers = $self->req->headers;
-  my $body = $self->req->body;
-  my $filename = $UPLOAD_DIR ."/". $headers->header("x-file-name");
-  warn "[!] Uploading file to $filename";
+	my $headers = $self->req->headers;
+	my $body = $self->req->body;
+	my $filename = $UPLOAD_DIR ."/". $headers->header("x-file-name");
+	warn "[!] Uploading file to $filename";
 
 	my $asset = Mojo::Asset::File->new;
-  $asset->add_chunk($body);
-  $asset->move_to($filename);
+	$asset->add_chunk($body);
+	$asset->move_to($filename);
 
 	$self->render_text('ok');
 };
