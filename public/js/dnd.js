fba67ed404e04bc848bc3bca57771a179b6c0a4f
Alexander Sapozhnikov <shoorick@mail.ru>
2018-03-09 00:59:51 +0500

    Function handleUpload can process more than one file

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -8,18 +8,20 @@ function uploadAsync(elem, url) {
 		console.log("file upload handled");
 		supressEvent(e);
 
-		var file = e.dataTransfer.files[0],
-		xhr = new XMLHttpRequest(),
-		name = file.name || file.fileName,
-		size = file.size || file.fileSize;
-		console.log(file);
+		for (i = 0; i < e.dataTransfer.files.length; i++) {
+			var file = e.dataTransfer.files[i],
+			xhr = new XMLHttpRequest(),
+			name = file.name || file.fileName,
+			size = file.size || file.fileSize;
+			console.log(file);
 
-		xhr.open("POST", url + name, true);
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		xhr.setRequestHeader("X-File-Name", name);
-		xhr.setRequestHeader("X-File-Size", size);
-		xhr.send(file);
-  }
+			xhr.open("POST", url + name, true);
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xhr.setRequestHeader("X-File-Name", name);
+			xhr.setRequestHeader("X-File-Size", size);
+			xhr.send(file);
+		}
+	}
 
 	fileForm.addEventListener('dragenter', supressEvent, false);
 	fileForm.addEventListener('dragexit', supressEvent, false);
